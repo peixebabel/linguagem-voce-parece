049e48089c8e8cdd9558855833899a35f069c5b0
Peixe Babel <canalpeixebabel@gmail.com>
2020-06-03 13:00:19 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 import django_heroku
 django_heroku.settings(locals())
