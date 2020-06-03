b2bbfd59ed573d5366cbd2a13e68320efe707bed
Peixe Babel <canalpeixebabel@gmail.com>
2020-06-03 12:46:56 -0300

    Update github_stereotype.py

--- github_stereotype.py
+++ github_stereotype.py
@@ -24,7 +24,7 @@ import matplotlib.pyplot as plt
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linguagem-voce-parece.settings")
 from django.core.management import execute_from_command_line
 execute_from_command_line(sys.argv)
 
